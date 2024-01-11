b8a9b9f79dd9132e4ae8cd0897d3a90972884ef7
Poowerllz <safilhoelzevirsafilho@gmail.com>
2024-01-11 14:07:18 -0300

    fix: start route

--- src/screens/SplashScreen/index.tsx
+++ src/screens/SplashScreen/index.tsx
@@ -38,10 +38,10 @@ const SplashScreen: React.FC = () => {
         selectCoach(currentCoach);
         selectSchool(currentSchool);
 
-        if (!currentCoach) {
-          navigate(PathRoutes.login.main, {replace: true});
-        } else if (!currentSchool) {
+        if (!currentSchool) {
           navigate(PathRoutes.selectSchool, {replace: true});
+        } else if (!currentCoach) {
+          navigate(PathRoutes.selectAccount, {replace: true});
         } else {
           navigate(PathRoutes.home.main, {replace: true});
         }
